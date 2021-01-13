674bb29890b4b74b13625fddd461fa272ff92c0d
Lostris Steekelenburg <lostristroi@gmail.com>
2021-01-13 15:58:00 +0100

    Je loopt nu automatisch naar 1 van de planes -> hieruit verder proberen om de plane uit te lezen

--- js/puzzel_1.js
+++ js/puzzel_1.js
@@ -1,7 +1,16 @@
 window.onload = () =>{
-  const cursor = document.getElementById('js--cursor-puzzel_1');
-  const keuze = document.getElementById('js--keuze');
-  cursor.addEventListener("mouseenter", Function(){
+  const places = document.getElementsByClassName('js--place');
+  const camera = document.getElementById('js--puzzel_1-camera');
 
-  });
+  let scene = document.getElementById('js--scene');
+
+
+  for(let i = 0; i < places.length; i++){
+    places[i].addEventListener('click', function(evt){
+      //camera.setAttribute('position', this.getAttribute('position').x + ' 1.6 ' + this.getAttribute('position').z)
+      let att = document.createAttribute("animation");
+      att.value = "property: position; easing: linear; dur: 2000; to: " + this.getAttribute("position").x + " 1.6 " + this.getAttribute("position").z;
+      camera.setAttribute("animation", att.value);
+    });
   }
+};
