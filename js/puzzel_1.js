360b6f271a18a0846d1349f48593ce3b2b3c6765
Lostris Steekelenburg <lostristroi@gmail.com>
2021-01-13 16:02:12 +0100

    Bij puzzel_1.html zijn nu nieuwe classnames toegevoegd. en bij puzzel_1.js is de javascript zo aangepast dat er nu hallo in de console verschijnt wanneer je over de plane hovered.

--- js/puzzel_1.js
+++ js/puzzel_1.js
@@ -6,11 +6,8 @@ window.onload = () =>{
 
 
   for(let i = 0; i < places.length; i++){
-    places[i].addEventListener('click', function(evt){
-      //camera.setAttribute('position', this.getAttribute('position').x + ' 1.6 ' + this.getAttribute('position').z)
-      let att = document.createAttribute("animation");
-      att.value = "property: position; easing: linear; dur: 2000; to: " + this.getAttribute("position").x + " 1.6 " + this.getAttribute("position").z;
-      camera.setAttribute("animation", att.value);
+    places[i].addEventListener('mouseenter', function(evt){
+      console.log("hallo");
     });
   }
 };
