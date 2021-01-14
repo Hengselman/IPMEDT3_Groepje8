752ec30f03265c2968b1c98a98d455ab1a5ffc0e
Lostris Steekelenburg <lostristroi@gmail.com>
2021-01-14 15:03:41 +0100

    puzzel_1.js en puzzel_1.html zijn aangepast waardoor het nu mogelijk is om doormiddel van kleuren een schrift / taal te selecteren of te deselecteren.
    Co-Authored-By: null <s1122255@users.noreply.github.com>

--- js/puzzel_1.js
+++ js/puzzel_1.js
@@ -1,17 +1,53 @@
 window.onload = () =>{
-  var keuzes = document.getElementsByClassName('js--keuze');
-  var schrift = document.getElementById('js--keuze-schrift');
-  var taal = document.getElementById('js--keuze-taal')
+  var taal = document.getElementsByClassName('js--keuze-taal');
+  var schrift = document.getElementsByClassName('js--keuze-schrift');
 
-  for(let i = 0; i < keuzes.length; i++){
-    keuzes[i].onmouseenter = (event) => {
-      keuze = keuzes[i].getAttribute("value");
-      keuzes[i].setAttribute("color","yellow");
-      console.log(keuze);
-
+function taalKleurYes(){
+  for(let i = 0; i < taal.length; i++){
+    taal[i].onmouseenter = (event) => {
+      taalKeuze = taal[i].getAttribute("value");
+      taalKleur = taal[i].getAttribute("color");
+      if(taalKleur == "yellow"){
+        taal[i].setAttribute("color","lightgrey");
+        taalKleur = taal[i].getAttribute("color");
+      }
+      else if(taalKleur == "lightgrey"){
+        taal[i].setAttribute("color","yellow");
+        taalKleur = taal[i].getAttribute("color");
+      }
+      console.log(taalKeuze);
+      console.log(taalKleur);
     }
-
   }
+}
+taalkleur = taalKleurYes();
 
 
+function schriftKleurYes(){
+  for(let i = 0; i < schrift.length; i++){
+    schrift[i].onmouseenter = (event) => {
+      schriftKeuze = schrift[i].getAttribute("value");
+      schriftKleur = schrift[i].getAttribute("color");
+      if(schriftKleur == "blue"){
+        schrift[i].setAttribute("color","lightgrey");
+        schriftKleur = schrift[i].getAttribute("color");
+      }
+      else if(schriftKleur == "lightgrey"){
+        schrift[i].setAttribute("color","blue");
+        schriftKleur = schrift[i].getAttribute("color");
+      }
+      console.log(schriftKeuze);
+      console.log(schriftKleur);
+    }
+  }
+}
+
+schriftkleur = schriftKleurYes();
+
+function isJuist(taalkleur, schriftkleur){
+  console.log("taalkleur is: " + taalkleur);
+  console.log("schriftkleur is: " + schriftkleur);
+}
+
+isJuist(taalkleur, schriftkleur);
 }
