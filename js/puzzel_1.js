8235b0a85d4bc57b9f0e792a6bc4f9b5a10010ad
Lostris Steekelenburg <lostristroi@gmail.com>
2021-01-14 13:14:40 +0100

    Puzzel_1.html is verbeterd samen met puzzel_1.js -> nu is het mogelijk om alle verschillende vakken uit te lezen en dit wordt gelogd naar de console.

--- js/puzzel_1.js
+++ js/puzzel_1.js
@@ -1,21 +1,18 @@
 window.onload = () =>{
-  const places = document.getElementsByClassName('js--place');
+  var places = document.getElementsByClassName('js--place');
   const camera = document.getElementById('js--puzzel_1-camera');
-
+  var keuze = document.getElementById('js--keuze');
   let scene = document.getElementById('js--scene');
 
 
-
-};
-
-function keuze(){
-  const places = document.getElementsByClassName('js--place');
   for(let i = 0; i < places.length; i++){
-    places[i].addEventListener('mouseenter', function(evt){
-      value = document.getElementById('js--tekst').getAttribute('value');
-      console.log(value);
-    });
-  }
-}
+    places[i].onmouseenter = (event) => {
+      brush = places[i].getAttribute("value");
+      console.log(brush);
 
-keuze();
+    }
+
+  }
+
+
+}
