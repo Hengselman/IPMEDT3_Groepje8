c53f721a5f3c1a12773f50caa08f0ddeb93a217c
Lostris Steekelenburg <lostristroi@gmail.com>
2021-01-20 12:01:20 +0100

    Puzzel_4.js en puzzel_4.html aangepast waardoor je nu de letter te zien krijgt als je correct antwoord.

--- js/puzzel_4.js
+++ js/puzzel_4.js
@@ -1,24 +1,31 @@
 window.onload = () =>{
   var keuze = document.getElementsByClassName('js--keuze');
+  var correct = document.getElementsByClassName('js--correct');
 
   for(let j = 0; j < keuze.length; j++){
     keuze[j].onmouseenter = (event) => {
       gemaakteKeuze = keuze[j].getAttribute("value");
       keuzeKleur = keuze[j].getAttribute("color");
-      if(keuzeKleur == "lightblue" || keuzeKleur == "red" || keuzeKleur == "green"){
+      if(keuzeKleur == "red" || keuzeKleur == "green"){
         keuze[j].setAttribute("color","lightgrey");
         keuzeKleur = keuze[j].getAttribute("color");
       }
-      else if(keuzeKleur == "lightgrey"){
-        keuze[j].setAttribute("color","lightblue");
-        keuzeKleur = keuze[j].getAttribute("color");
         if(gemaakteKeuze == "onjuist"){
           keuze[j].setAttribute("color","red");
         }else{
           keuze[j].setAttribute("color","green");
         }
+        keuzeKleur = keuze[j].getAttribute("color");
+        console.log(keuzeKleur);
+
+        for(let j = 0; j < correct.length; j++){
+          if(keuzeKleur == "green"){
+            correct[j].getAttribute("value");
+            correct[j].setAttribute("value", "Gefeliciteerd! je hebt de puzzel opgelost en hiervoor krijg je de volgende letter: T");
+          }
+        }
+
       }
+
     }
   }
-
-}
